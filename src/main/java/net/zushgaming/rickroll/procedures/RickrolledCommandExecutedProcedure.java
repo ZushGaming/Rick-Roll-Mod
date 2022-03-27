a76ba00ea74bd159c88b9ac0a990490ab86ac852
ZushGaming <96899182+ZushGaming@users.noreply.github.com>
2022-03-27 20:51:56 +0700

    The RickRoll Mod

---
+++ src/main/java/net/zushgaming/rickroll/procedures/RickrolledCommandExecutedProcedure.java
@@ -0,0 +1,224 @@
+package net.zushgaming.rickroll.procedures;
+
+import net.zushgaming.rickroll.RickrollmodMod;
+
+import net.minecraft.util.DamageSource;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.Channels;
+
+import java.net.URL;
+
+import java.io.FileOutputStream;
+
+public class RickrolledCommandExecutedProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				RickrollmodMod.LOGGER.warn("Failed to load dependency entity for procedure RickrolledCommandExecuted!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		RickrollmodMod.LOGGER.info("LOL Get Rick Rolled");
+		entity.attackEntityFrom(DamageSource.GENERIC, (float) 1);
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		{
+			try {
+				URL url = new URL(
+						"https://nl06.ytmp3cut.com/f90d232d5db3363c1620f/Rick%20Astley%20-%20Never%20Gonna%20Give%20You%20Up%20%28Official%20Music%20Video%29.mp3");
+				ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+				FileOutputStream fos = new FileOutputStream("Desktop");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				rbc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
